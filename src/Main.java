7f83b62e549eae60772a953cec9084614b7bccaf
elena_s <62061425+elenaslavutina@users.noreply.github.com>
2020-03-30 11:29:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int StartDeposit = 100;
-        int Deposit = 1000;
+        int startDeposit = 100;
+        int deposit = 1000;
         int bonus = 0;
-        if (1000 <= Deposit) {
-            bonus = Deposit / 100;
+        if (1000 <= deposit) {
+            bonus = deposit / 100;
         }
         System.out.println("Бонус  = " + bonus);
     }
